fd45c249230bb3721a3b785c985453de2a42fcd5
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-05-10 08:25:10 +0300

    add more recursion

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -107,23 +107,90 @@ void recurTreeHead(int n){
     }
 }
 
+// INDERECT RECURSION
+// multimple functions call each other in a circular fashion
+int recurInderectB(int b);
+void recurInderectA(int a){
+    if (a > 0){
+        printf("%d", a);
+        recurInderectB(a-1);
+    }
+}
+
+int recurInderectB(int b){
+    if ( b > 1){
+        recurInderectA(b/2);
+    }
+    return  0;
+}
+
+// NESTED RECURSION
+// outer function waits until inner returns
+int recurNested(int a){
+    printf("%d ", a);
+    if (a > 10){
+        return a - 1;
+    }
+    return recurNested(recurNested(a + 2));
+}
+
+// IMPLEMENTATIONS
+// sum of natural numbers: 1+2+3+4+5..
+// sum() base case: n = 0 --> 0
+// sum(): ( n - 1 ) + n
+// too costly for real world scenarios
+// return n * ( n + 1 ) / 2 --> constant time
+int sumOfNaturalNumbers(int a){
+    if (a > 0){
+        return sumOfNaturalNumbers(a - 1) + a;
+    }
+    return 0;
+}
+
+// Factorial
+int recurFactorial(int a){
+    if (a == 0){
+        return 1;
+    }
+    return recurFactorial(a - 1) * a;
+}
+
+int iterFactorial(int a){
+    if (a <= 1) {
+        return 1;
+    }
+    int n = 1;
+    for (int i = 1; i <= a; i++){
+       n = n * i;
+    }
+    return n;
+}
+
 int main(int argc, const char * argv[]) {
     int testVal = 5;
     //recurHead(testVal);
     //recurTail(testVal);
     //recurTreeHead(testVal);
-    
+    //recurInderectA(testVal);
+
     int stat1 = recurStatic(testVal);
     // NB! static stays 5 after the last recerStatic call
     int stat2 = recurStatic(testVal);
     
     int stack1 = recurStack(testVal);
+    //int nested = recurNested(testVal);
 
-
+    int sumNatural = sumOfNaturalNumbers(testVal);
+    int rfac = recurFactorial(testVal);
+    int ifac = iterFactorial(testVal);
+    
     printf("recurStatic result 1: %d\n", stat1);
     printf("recurStatic result 2: %d\n", stat2);
     printf("recurStack result 1: %d\n", stack1);
-
+    //printf("recurNested result 1: %d\n", nested);
+    printf("sumOfNaturalNumbers result 1: %d\n", sumNatural);
+    printf("recurFactorial result 1: %d\n", rfac);
+    printf("iterFactorial result 1: %d\n", ifac);
 
     return 0;
 }
